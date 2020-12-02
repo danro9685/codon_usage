e34a29cb401a77204a6a046f8493123c60118a94
danro9685 <daniele.ramazzotti@yahoo.com>
2020-12-02 10:59:20 +0100

    minor refactory

--- analysis/step1_data_preprocessing/main_part1_preprocessing_dataset1.R
+++ analysis/step1_data_preprocessing/main_part1_preprocessing_dataset1.R
@@ -1,7 +1,7 @@
 # read data
-annotations = read.delim("data/GCF_009858895.2_ASM985889v3_genomic.gff",header=FALSE,comment.char="#",check.names=FALSE,stringsAsFactors=FALSE)
-human_cu = read.table("data/HumanCodonUsage.txt",header=FALSE,check.names=FALSE,stringsAsFactors=FALSE)
-sars_cov2_fasta = read.table("data/SARS-CoV2.fasta",header=FALSE,sep="\n",check.names=FALSE,stringsAsFactors=FALSE)
+annotations = read.delim("external_data/GCF_009858895.2_ASM985889v3_genomic.gff",header=FALSE,comment.char="#",check.names=FALSE,stringsAsFactors=FALSE)
+human_cu = read.table("external_data/HumanCodonUsage.txt",header=FALSE,check.names=FALSE,stringsAsFactors=FALSE)
+sars_cov2_fasta = read.table("external_data/SARS-CoV2.fasta",header=FALSE,sep="\n",check.names=FALSE,stringsAsFactors=FALSE)
 sars_cov2_fasta = toupper(strsplit(paste0(sars_cov2_fasta[-1,],collapse=""),split="")[[1]])
 load(file="final_data/PRJEB37886/processed_variants.RData")
 
